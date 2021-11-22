f0279c646babd204e82df793d78088c0fd93db86
Thiago Santos <49589136+ThiagoSzz@users.noreply.github.com>
2021-11-22 11:21:41 -0300

    Delete convImage.m

--- Border based Segmentation/convImage.m
+++
@@ -1,9 +0,0 @@
-
-function blurredImage = convImage(image, kernel)
-    rChannel = imfilter(image(:,:,1), kernel, 'conv');
-    gChannel = imfilter(image(:,:,2), kernel, 'conv');
-    bChannel = imfilter(image(:,:,3), kernel, 'conv');
-    
-    blurredImage = cat(3, rChannel, gChannel, bChannel);
-end
-
