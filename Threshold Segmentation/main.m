9fb315f6de6b0b6a1a9ea942e026d7206d0f99d1
Thiago Santos <49589136+ThiagoSzz@users.noreply.github.com>
2021-11-23 00:45:29 -0300

    final final comit

--- Threshold Segmentation/main.m
+++ Threshold Segmentation/main.m
@@ -1,26 +1,43 @@
-path = 'C:\Users\User\Desktop\TrabalhoFPI\Threshold Segmentation\';
+% caminho da pasta aonde estão localizadas as imagens
+% modifique aqui
+path = 'C:\Users\User\Desktop\TrabalhoFPI\Threshold Segmentation\images\';
+
+% nomes dos arquivos das imagens utilizadas
 imagesPath = {append(path, 'carros.png'), append(path, 'mcdonalds.jpg')};
 
+% iteração sobre as imagens da lista
 for i=1:length(imagesPath)
     
     fprintf('\nProcess for image %d initialized.\n', i);
     
+    % imagem lida
     image = imread(imagesPath{i});
-    limiarizationNumber = 20;
+    
+    % número de n-regiões (ou seja, (n-1)-valores de limiar)
+    numberOfRegions = 20;
 
-    quantized = quantizeImage(image, limiarizationNumber);
+    % realiza a conversão, quantização e segmentação da imagem
+    quantized = quantizeImage(image, numberOfRegions);
     
     fprintf('Image %d segmented successfully.\n', i);
 
+    % inicializa variáveis úteis a mostragem
     mask = {};
     maskedImage = zeros(size(image,1), size(image,2));
     sumOfImages = zeros(size(image,1), size(image,2));
     image = double(image)/255;
     
+    % realiza a mostragem dos segmentos por meio da
+    % criação de uma máscara com os valores de intensidade segmentados
+    % e da multiplicação destes com a imagem original
+    
+    % após isso, o resultado desta operação
+    % é somado a uma imagem totalmente preta
     figure();
-        for j=1:limiarizationNumber
+        for j=1:numberOfRegions
             mask = buildMask(quantized, j);
 
+            % aplica-se a máscara em cada um dos canais da imagem
             maskedImage(:,:,1) = image(:,:,1).*mask;
             maskedImage(:,:,2) = image(:,:,2).*mask;
             maskedImage(:,:,3) = image(:,:,3).*mask;
@@ -38,3 +55,5 @@ for i=1:length(imagesPath)
     
     fprintf('Image %d printed successfully.\n', i);
 end
+
+fprintf('\nThreshold segmentation done!\n');
--- k-Means Segmentation/main.m
+++ k-Means Segmentation/main.m
@@ -1,19 +1,31 @@
-path = 'C:\Users\User\Desktop\TrabalhoFPI\k-Means Segmentation\';
+% caminho da pasta aonde estão localizadas as imagens
+% modifique aqui
+path = 'C:\Users\User\Desktop\TrabalhoFPI\k-Means Segmentation\images\';
+
+% nomes dos arquivos das imagens utilizadas
 imagesPath = {append(path, 'carros.png'), append(path, 'mcdonalds.jpg')};
 
+% iteração sobre as imagens da lista
 for i=1:length(imagesPath)
     
     fprintf('\nProcess for image %d initialized.\n', i);
     
-    image = double(imread(imagesPath{i})); % imagem para segmentar
+    % imagem lida e convertida para double
+    image = double(imread(imagesPath{i}));
 
-    k = 32; % numero de segmentos k-means
+    % número de k-segmentos para segmentação
+    k = 32;
+    
+    % executa o algoritmo de agrupamento
     [clusteredImg, labeledImg, codebook] = clusterImage(image, k);
+    
+    % coleta cada agrupamento em um vetor
     eachClusterImg = eachCluster(labeledImg, codebook);
     
     fprintf('Image %d segmented successfully.\n', i);
 
-    
+    % realiza a mostragem dos segmentos por meio da
+    % soma destes a uma imagem totalmente preta
     figure();
         for j = 1:k
             if(j == 1)
@@ -33,3 +45,5 @@ for i=1:length(imagesPath)
     
     fprintf('Image %d printed successfully.\n', i);
 end
+
+fprintf('\nK-means segmentation done!\n');
